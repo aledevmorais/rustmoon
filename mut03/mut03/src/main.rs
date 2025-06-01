2d4961411fde349bc0e4e6e40e2260425a5773eb
aledevmorais <aledevmorais@gmail.com>
2025-06-01 19:10:05 -0300

    dinovo

--- mut03/mut03/src/main.rs
+++ mut03/mut03/src/main.rs
@@ -12,4 +12,10 @@ fn main() {
     y = 6;
     println!("O valor de y agora é: {y}");
 
+    //essa é esquematização de reatribuição de valor de uma let (mutável)
+    // o cargo run emitirá um warning pois a x =6; não foi utilizada e abaixo o run fez o println!
+    // O valor de x é: 5
+    // O valor de x agora é: 666
+    // O valor de y é: 5
+    // O valor de y agora é: 6
 }
