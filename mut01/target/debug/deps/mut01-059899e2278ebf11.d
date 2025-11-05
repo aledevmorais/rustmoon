5380363721379098c899e4be0a5280d6f3da44c0
aledevmorais <transpdev@gmail.com>
2025-11-05 17:30:54 -0300

    release

---
+++ mut01/target/debug/deps/mut01-059899e2278ebf11.d
@@ -0,0 +1,5 @@
+C:\abyrust\rustmoon\rustmoon\mut01\target\debug\deps\libmut01-059899e2278ebf11.rmeta: src\main.rs
+
+C:\abyrust\rustmoon\rustmoon\mut01\target\debug\deps\mut01-059899e2278ebf11.d: src\main.rs
+
+src\main.rs:
---
+++ mut01/target/debug/deps/mut01-d12e37a617c11a0a.d
@@ -0,0 +1,5 @@
+C:\abyrust\rustmoon\rustmoon\mut01\target\debug\deps\libmut01-d12e37a617c11a0a.rmeta: src\main.rs
+
+C:\abyrust\rustmoon\rustmoon\mut01\target\debug\deps\mut01-d12e37a617c11a0a.d: src\main.rs
+
+src\main.rs:
